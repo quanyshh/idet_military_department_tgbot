53af32a26e2cf16296452aac453f354f90fb1863
quanyshh work <quanyshh@gmail.com>
2022-03-18 11:41:52 +0600

    add standarts and logo

--- handlers/users/menu_handlers.py
+++ handlers/users/menu_handlers.py
@@ -4,9 +4,8 @@ from aiogram import types
 from aiogram.dispatcher.filters import Command
 
 from keyboards.inline.menu_keyboards import language_keyboard, main_menu_keyboard, questions_keyboard, question_keyboard, menu_cd
-from loader import dp
+from loader import dp, bot
 from utils.db_api.database import data
-
 
 @dp.message_handler(Command("menu"))
 async def show_menu(message: types.Message):
@@ -35,11 +34,12 @@ async def list_questions(callback: types.CallbackQuery, language, main_menu, **k
 async def show_question(callback: types.CallbackQuery, language, main_menu, questions, question_id):
     markup = question_keyboard(language, main_menu, questions, question_id)
     text = f"{data[questions]}"
+    chat_id = callback.message['chat']['id']
     id = int(questions[-1])
-    # if id == 8:
-    #     await callback.message.send_photo(photo=open('btn8_kaz.png', 'rb'))
-    # else:
-    await callback.message.edit_text(text, reply_markup=markup, parse_mode="HTML")
+    if id == 8:
+        await bot.send_photo(chat_id = chat_id, photo=open(f'data/{language}_standards.png', 'rb'))
+    else:
+        await callback.message.edit_text(text, reply_markup=markup, parse_mode="HTML")
 
 
 @dp.callback_query_handler(menu_cd.filter())
@@ -49,7 +49,6 @@ async def navigate(call: types.CallbackQuery, callback_data: dict):
     main_menu = callback_data.get('main_menu')
     questions = callback_data.get('questions')
     question_id = callback_data.get('question_id')
-
 
     levels = {
         "0": list_of_languages,
