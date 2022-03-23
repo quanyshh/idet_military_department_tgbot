b79538efea384b64e3f3b6e72c908bbf0464e9ad
quanyshh work <quanyshh@gmail.com>
2022-03-24 02:55:20 +0600

    add username to log file

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,7 +7,7 @@ import logging
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
 
-    logging.info(f"id={message.from_user.id}, full_name={message.from_user.full_name}, locale={message.from_user.locale}, mention={message.from_user.mention}")
+    logging.info(f"id={message.from_user.id}, full_name={message.from_user.full_name}, locale={message.from_user.locale}, mention={message.from_user.mention},  username={message.from_user.username}")
 
    
     await message.answer(f"Привет, {message.from_user.full_name}! Нажми /menu")
